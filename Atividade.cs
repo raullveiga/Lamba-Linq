55ae040c3376d74f86e7cd4e08c3246c47529707
Raul <raullalvarenga@hotmail.com>
2017-11-24 21:55:02 -0200

    Fim da aula

---
+++ Atividade.cs
@@ -0,0 +1,13 @@
+namespace lambda
+{
+    public abstract class Atividade
+    {
+        public static double subtrair(int v1,int v2){
+            return v1-v2;
+        }
+        public static string Multiplicar(int v1, int v2){
+            return (v1*v2).ToString();
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using lambda;
+using System;
+
+namespace lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Func<int,int,double> total = Atividade.subtrair; 
+            System.Console.WriteLine(total(10,45));
+            Func<int,int,string> sdg = Atividade.Multiplicar;
+            System.Console.WriteLine(sdg(23,4));
+           
+            Data dt = delegate(string msg){return msg+" batata ~ "+DateTime.Now;};
+            System.Console.WriteLine(dt("lksa")); */
+
+            int[] valores = {34,34,54,1,46,76,34,65,23};
+            
+
+
+            /* Func<int,bool> rs = delegate(int x){return x % 2 == 0;};
+            
+
+            foreach(var i in valores)
+                if(rs(i))
+                    System.Console.WriteLine(i); */
+        
+
+
+
+            /* Func<int,bool> rs = x => x % 2 == 0;
+            
+
+            foreach(var i in valores)
+                if(rs(i))
+                    System.Console.WriteLine(i); */
+
+            Func<double,double,double> calculo = (x,y) => x/y;
+            Console.WriteLine(calculo(31,43));
+        }
+        
+        static bool par(int v){
+            return v%2==0;
+        }
+
+        delegate string Data(string mensagem);
+
+    }
+}
